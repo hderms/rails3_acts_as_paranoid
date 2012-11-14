0a558c5fea5a84a315b5fca0a7d5f184b91349f8
Dermot Haughey <dermot.haughey@rvibe.com>
2012-11-14 16:04:54 -0600

    Change formatting and allow someone to skip acts_as_paranoid in suclasses

--- lib/rails3_acts_as_paranoid.rb
+++ lib/rails3_acts_as_paranoid.rb
@@ -8,42 +8,47 @@ require 'acts_as_paranoid/relation'
 
 
 module ActsAsParanoid
-  
+
   def paranoid?
     self.included_modules.include?(ActsAsParanoid::Core)
   end
-  
+
   def validates_as_paranoid
     include ActsAsParanoid::Validations
   end
-  
+  def skip_acts_as_paranoid
+    class_attribute :paranoid_skipped
+    self.paranoid_skipped
+  end
+
   def acts_as_paranoid(options = {})
     raise ArgumentError, "Hash expected, got #{options.class.name}" if not options.is_a?(Hash) and not options.empty?
-    
-    class_attribute :paranoid_configuration, :paranoid_column_reference
-    
-    self.paranoid_configuration = { :column => "deleted_at", :column_type => "time", :recover_dependent_associations => true, :dependent_recovery_window => 2.minutes }
-    self.paranoid_configuration.merge!({ :deleted_value => "deleted" }) if options[:column_type] == "string"
-    self.paranoid_configuration.merge!(options) # user options
+    unless self.paranoid_skipped?
+      class_attribute :paranoid_configuration, :paranoid_column_reference
 
-    raise ArgumentError, "'time', 'boolean' or 'string' expected for :column_type option, got #{paranoid_configuration[:column_type]}" unless ['time', 'boolean', 'string'].include? paranoid_configuration[:column_type]
+      self.paranoid_configuration = { :column => "deleted_at", :column_type => "time", :recover_dependent_associations => true, :dependent_recovery_window => 2.minutes }
+      self.paranoid_configuration.merge!({ :deleted_value => "deleted" }) if options[:column_type] == "string"
+      self.paranoid_configuration.merge!(options) # user options
 
-    self.paranoid_column_reference = "#{self.table_name}.#{paranoid_configuration[:column]}"
-    
-    return if paranoid?
-    
-    include ActsAsParanoid::Core
-    
-    # Magic!
-    default_scope { where(paranoid_default_scope_sql) }
+      raise ArgumentError, "'time', 'boolean' or 'string' expected for :column_type option, got #{paranoid_configuration[:column_type]}" unless ['time', 'boolean', 'string'].include? paranoid_configuration[:column_type]
 
-    if paranoid_configuration[:column_type] == 'time'
-      scope :deleted_inside_time_window, lambda {|time, window|
-        deleted_after_time((time - window)).deleted_before_time((time + window))
-      }
+      self.paranoid_column_reference = "#{self.table_name}.#{paranoid_configuration[:column]}"
 
-      scope :deleted_after_time, lambda  { |time| where("#{paranoid_column} > ?", time) }
-      scope :deleted_before_time, lambda { |time| where("#{paranoid_column} < ?", time) }
+      return if paranoid?
+
+      include ActsAsParanoid::Core
+
+      # Magic!
+      default_scope { where(paranoid_default_scope_sql) }
+
+      if paranoid_configuration[:column_type] == 'time'
+        scope :deleted_inside_time_window, lambda {|time, window|
+          deleted_after_time((time - window)).deleted_before_time((time + window))
+        }
+
+        scope :deleted_after_time, lambda  { |time| where("#{paranoid_column} > ?", time) }
+        scope :deleted_before_time, lambda { |time| where("#{paranoid_column} < ?", time) }
+      end
     end
   end
 end
